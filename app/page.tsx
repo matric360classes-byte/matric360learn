0f99910127ae46d82a4a745191a4932f7f32b851
matric360classes-byte <matric360classes@gmail.com>
2026-08-27 17:00:34 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+return <div style={{padding:'40px',background:'#000',color:'#fff',minHeight:'100vh',fontFamily:'system-ui'}}>
+<h1>matric360learn.co.za is LIVE</h1>
+<p>1 Subject: R149/m (R1,490/yr)</p>
+<p>2 Subjects: R249/m (R2,490/yr) - Most Popular</p>
+<p>3=R329 | 4=R399 | All=R499</p>
+<p>Easy-add subjects: 1 line config</p>
+</div>
+}
