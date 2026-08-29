5da3d32df14d70578bd3003120428d94684c8a81
matric360classes-byte <matric360classes@gmail.com>
2026-08-29 19:31:15 +0200

    Update page.tsx

--- app/subjects/[id]/page.tsx
+++ app/subjects/[id]/page.tsx
@@ -1,1 +1,1 @@
-export default function Page(){return <div style={{background:"#0f0f13",minHeight:"100vh",color:"white",padding:20}}>Level 2 OK - Fixed</div>}
+export default function Page(){return <div>Level 2 OK</div>}
