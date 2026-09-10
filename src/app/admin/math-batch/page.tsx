1bd40f8c8cd689cafcb69a7eefa96fd15f172a75
matric360classes-byte <matric360classes@gmail.com>
2026-09-10 20:33:19 +0200

    Update page.tsx

--- src/app/admin/math-batch/page.tsx
+++ src/app/admin/math-batch/page.tsx
@@ -1,5 +1,4 @@
-import MathBatch from "../../../components/admin/MathBatch";
-
-export default function Page(){
-  return <MathBatch />;
-}
+"use client";
+import dynamic from "next/dynamic";
+const MathBatch = dynamic(() => import("../../../components/admin/MathBatch"), { ssr: false });
+export default function Page(){ return <MathBatch />; }
