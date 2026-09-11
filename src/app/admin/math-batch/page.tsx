0ac5d6c53078e4dba68b425357b888961a3a2b4c
matric360classes-byte <matric360classes@gmail.com>
2026-09-11 05:00:13 +0200

    Delete src/app/admin/math-batch directory

--- src/app/admin/math-batch/page.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const MathBatch = dynamic(() => import("../../../components/admin/MathBatch"), { ssr: false });
-export default function Page(){ return <MathBatch />; }
